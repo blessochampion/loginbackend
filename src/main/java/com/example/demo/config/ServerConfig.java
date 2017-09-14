787f55133436fc6d75ce297ae302062773e52372
Blessing Ekundayo <Blessing.Ekundayo@Interswitchng.com>
2017-09-14 13:13:45 +0100

    database update

--- src/main/java/com/example/demo/config/ServerConfig.java
+++ src/main/java/com/example/demo/config/ServerConfig.java
@@ -2,6 +2,8 @@ package com.example.demo.config;
 
 public class ServerConfig
 {
+
+
     public static final String EMAIL = "loginappng@gmail.com";
     public static final String EMAIL_TITLE = "Account Registration confirmation";
 }
